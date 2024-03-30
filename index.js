59031dc5ea8040fd06931ff056779e1c974c490a
Lorraine Opiyo <achienglorraine8@gmail.com>
2024-03-30 16:15:20 +0300

    phase-1-array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-// code your solution here
+function superbowlWin(superbowls) {
+    const winningYearObj = superbowls.find(superbowl => superbowl.result === "W");
+    return winningYearObj ? winningYearObj.year : undefined;
+}
